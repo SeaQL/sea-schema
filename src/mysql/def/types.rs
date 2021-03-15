366e02220192ec6cb45f0b2af6030c62e1d46a2b
Chris Tsang <tyt2y7@gmail.com>
2021-03-15 23:59:18 +0800

    Column Type parser

--- src/mysql/def/types.rs
+++ src/mysql/def/types.rs
@@ -26,7 +26,7 @@ pub enum Type {
     TinyText(StringAttr),
     MediumText(StringAttr),
     LongText(StringAttr),
-    Blob(BinaryAttr),
+    Blob(BlobAttr),
     TinyBlob,
     MediumBlob,
     LongBlob,
@@ -41,44 +41,49 @@ pub enum Type {
     MultiPolygon(GeometryAttr),
     GeometryCollection(GeometryAttr),
     Json,
+    Unknown(String),
 }
 
 #[derive(Debug, Default)]
 pub struct NumericAttr {
-    maximum: Option<u32>,
-    decimal: Option<u32>,
-    unsigned: Option<bool>,
-    zero_fill: Option<u32>,
+    pub maximum: Option<u32>,
+    pub decimal: Option<u32>,
+    pub unsigned: Option<bool>,
+    pub zero_fill: Option<bool>,
 }
 
 #[derive(Debug, Default)]
 pub struct TimeAttr {
-    fractional: Option<u32>,
+    pub fractional: Option<u32>,
 }
 
 #[derive(Debug, Default)]
 pub struct StringAttr {
-    length: Option<u32>,
-    charset_name: Option<String>,
-    collation_name: Option<String>,
+    pub length: Option<u32>,
+    pub charset_name: Option<String>,
+    pub collation_name: Option<String>,
 }
 
 #[derive(Debug, Default)]
-pub struct BinaryAttr {
-    length: Option<u32>,
+pub struct BlobAttr {
+    pub length: Option<u32>,
 }
 
 #[derive(Debug, Default)]
 pub struct EnumDef {
-    values: Vec<String>,
-    attr: StringAttr,
+    pub values: Vec<String>,
+    pub attr: StringAttr,
 }
 
-pub type SetDef = EnumDef;
+#[derive(Debug, Default)]
+pub struct SetDef {
+    pub members: Vec<String>,
+    pub attr: StringAttr,
+}
 
 #[derive(Debug, Default)]
 pub struct GeometryAttr {
-    srid: Option<u32>,
+    pub srid: Option<u32>,
 }
 
 impl Type {
@@ -99,13 +104,19 @@ impl Type {
         }
     }
 
-    pub fn is_date_time(&self) -> bool {
+    pub fn is_date(&self) -> bool {
         match self {
             Type::Date => true,
+            Type::Year => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_time(&self) -> bool {
+        match self {
             Type::Time(_) => true,
             Type::DateTime(_) => true,
             Type::Timestamp(_) => true,
-            Type::Year => true,
             _ => false,
         }
     }
@@ -126,7 +137,7 @@ impl Type {
         }
     }
 
-    pub fn is_binary(&self) -> bool {
+    pub fn is_blob(&self) -> bool {
         match self {
             Type::Blob(_) => true,
             Type::TinyBlob => true,
@@ -136,7 +147,14 @@ impl Type {
         }
     }
 
-    pub fn is_spatial(&self) -> bool {
+    pub fn is_free_size_blob(&self) -> bool {
+        match self {
+            Type::Blob(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_geometry(&self) -> bool {
         match self {
             Type::Geometry(_) => true,
             Type::Point(_) => true,
@@ -150,9 +168,15 @@ impl Type {
         }
     }
 
-    pub fn is_aggregate(&self) -> bool {
+    pub fn is_enum(&self) -> bool {
         match self {
             Type::Enum(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_set(&self) -> bool {
+        match self {
             Type::Set(_) => true,
             _ => false,
         }
@@ -162,6 +186,90 @@ impl Type {
         match self {
             Type::Json => true,
             _ => false,
+        }
+    }
+
+    pub fn is_unknown(&self) -> bool {
+        match self {
+            Type::Unknown(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn get_numeric_attr_mut(&mut self) -> &mut NumericAttr {
+        match self {
+            Type::Serial(attr) => attr,
+            Type::Bit(attr) => attr,
+            Type::TinyInt(attr) => attr,
+            Type::Bool(attr) => attr,
+            Type::SmallInt(attr) => attr,
+            Type::MediumInt(attr) => attr,
+            Type::Int(attr) => attr,
+            Type::BigInt(attr) => attr,
+            Type::Decimal(attr) => attr,
+            Type::Float(attr) => attr,
+            Type::Double(attr) => attr,
+            _ => panic!("type error"),
+        }
+    }
+
+    pub fn get_time_attr_mut(&mut self) -> &mut TimeAttr {
+        match self {
+            Type::Time(attr) => attr,
+            Type::DateTime(attr) => attr,
+            Type::Timestamp(attr) => attr,
+            _ => panic!("type error"),
+        }
+    }
+
+    pub fn get_string_attr_mut(&mut self) -> &mut StringAttr {
+        match self {
+            Type::Char(attr) => attr,
+            Type::NChar(attr) => attr,
+            Type::Varchar(attr) => attr,
+            Type::NVarchar(attr) => attr,
+            Type::Binary(attr) => attr,
+            Type::Varbinary(attr) => attr,
+            Type::Text(attr) => attr,
+            Type::TinyText(attr) => attr,
+            Type::MediumText(attr) => attr,
+            Type::LongText(attr) => attr,
+            _ => panic!("type error"),
+        }
+    }
+
+    pub fn get_blob_attr_mut(&mut self) -> &mut BlobAttr {
+        match self {
+            Type::Blob(attr) => attr,
+            _ => panic!("type error"),
+        }
+    }
+
+    pub fn get_enum_def_mut(&mut self) -> &mut EnumDef {
+        match self {
+            Type::Enum(def) => def,
+            _ => panic!("type error"),
+        }
+    }
+
+    pub fn get_set_def_mut(&mut self) -> &mut SetDef {
+        match self {
+            Type::Set(def) => def,
+            _ => panic!("type error"),
+        }
+    }
+
+    pub fn get_geometry_attr_mut(&mut self) -> &mut GeometryAttr {
+        match self {
+            Type::Geometry(attr) => attr,
+            Type::Point(attr) => attr,
+            Type::LineString(attr) => attr,
+            Type::Polygon(attr) => attr,
+            Type::MultiPoint(attr) => attr,
+            Type::MultiLineString(attr) => attr,
+            Type::MultiPolygon(attr) => attr,
+            Type::GeometryCollection(attr) => attr,
+            _ => panic!("type error"),
         }
     }
 }
--- src/mysql/parser/mod.rs
+++ src/mysql/parser/mod.rs
@@ -14,7 +14,280 @@ pub fn parse_column_query_result(result: ColumnQueryResult) -> ColumnInfo {
 }
 
 pub fn parse_column_type(column_type: String) -> ColumnType {
-    todo!()
+    let tokenizer = Tokenizer::new(column_type.as_str());
+    let mut tokens: Vec<Token> = tokenizer.iter()
+        .filter(|x| !x.is_space()) // retains non-space tokens
+        .collect::<Vec<_>>().into_iter().rev().collect(); // reverse the vector so pop() is actually at front
+
+    let mut type_name = "";
+    if tokens.is_empty() {
+        return Type::Unknown(column_type);
+    }
+    let tok = tokens.pop().unwrap();
+    if tok.is_unquoted() {
+        type_name = tok.as_str();
+    }
+    let ctype = parse_type_name(type_name);
+    if ctype.is_numeric() {
+        parse_numeric_attributes(tokens, ctype)
+    } else if ctype.is_time() {
+        parse_time_attributes(tokens, ctype)
+    } else if ctype.is_string() {
+        parse_string_attributes(tokens, ctype)
+    } else if ctype.is_free_size_blob() {
+        parse_blob_attributes(tokens, ctype)
+    } else if ctype.is_enum() {
+        parse_enum_definition(tokens, ctype)
+    } else if ctype.is_set() {
+        parse_set_definition(tokens, ctype)
+    } else if ctype.is_geometry() {
+        parse_geometry_attributes(tokens, ctype)
+    } else {
+        ctype
+    }
+}
+
+pub fn parse_type_name(type_name: &str) -> Type {
+    match type_name.to_lowercase().as_str() {
+        "serial" => Type::Serial(NumericAttr::default()),
+        "bit" => Type::Bit(NumericAttr::default()),
+        "tinyint" => Type::TinyInt(NumericAttr::default()),
+        "bool" => Type::Bool(NumericAttr::default()),
+        "smallint" => Type::SmallInt(NumericAttr::default()),
+        "mediumint" => Type::MediumInt(NumericAttr::default()),
+        "int" => Type::Int(NumericAttr::default()),
+        "bigint" => Type::BigInt(NumericAttr::default()),
+        "decimal" => Type::Decimal(NumericAttr::default()),
+        "float" => Type::Float(NumericAttr::default()),
+        "double" => Type::Double(NumericAttr::default()),
+        "date" => Type::Date,
+        "time" => Type::Time(TimeAttr::default()),
+        "datetime" => Type::DateTime(TimeAttr::default()),
+        "timestamp" => Type::Timestamp(TimeAttr::default()),
+        "year" => Type::Year,
+        "char" => Type::Char(StringAttr::default()),
+        "nchar" => Type::NChar(StringAttr::default()),
+        "varchar" => Type::Varchar(StringAttr::default()),
+        "nvarchar" => Type::NVarchar(StringAttr::default()),
+        "binary" => Type::Binary(StringAttr::default()),
+        "varbinary" => Type::Varbinary(StringAttr::default()),
+        "text" => Type::Text(StringAttr::default()),
+        "tinytext" => Type::TinyText(StringAttr::default()),
+        "mediumtext" => Type::MediumText(StringAttr::default()),
+        "longtext" => Type::LongText(StringAttr::default()),
+        "blob" => Type::Blob(BlobAttr::default()),
+        "tinyblob" => Type::TinyBlob,
+        "mediumblob" => Type::MediumBlob,
+        "longblob" => Type::LongBlob,
+        "enum" => Type::Enum(EnumDef::default()),
+        "set" => Type::Set(SetDef::default()),
+        "geometry" => Type::Geometry(GeometryAttr::default()),
+        "point" => Type::Point(GeometryAttr::default()),
+        "linestring" => Type::LineString(GeometryAttr::default()),
+        "polygon" => Type::Polygon(GeometryAttr::default()),
+        "multipoint" => Type::MultiPoint(GeometryAttr::default()),
+        "multilinestring" => Type::MultiLineString(GeometryAttr::default()),
+        "multipolygon" => Type::MultiPolygon(GeometryAttr::default()),
+        "geometrycollection" => Type::GeometryCollection(GeometryAttr::default()),
+        "json" => Type::Json,
+        _ => Type::Unknown(type_name.to_owned()),
+    }
+}
+
+fn parse_numeric_attributes(mut tokens: Vec<Token>, mut ctype: ColumnType) -> ColumnType {
+    if tokens.is_empty() { return ctype; }
+    let tok = tokens.pop().unwrap();
+
+    if tok.is_punctuation() && tok.as_str() == "(" {
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if tok.is_unquoted() && tok.as_str().parse::<u32>().is_ok() {
+            ctype.get_numeric_attr_mut().maximum = Some(tok.as_str().parse::<u32>().unwrap());
+        }
+
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if tok.is_punctuation() && tok.as_str() == "," {
+            if tokens.is_empty() { return ctype; }
+            let tok = tokens.pop().unwrap();
+
+            if tok.is_unquoted() && tok.as_str().parse::<u32>().is_ok() {
+                ctype.get_numeric_attr_mut().decimal = Some(tok.as_str().parse::<u32>().unwrap());
+            }
+        }
+
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if !(tok.is_punctuation() && tok.as_str() == ")") { return ctype; }
+    }
+
+    if tok.is_unquoted() && tok.as_str().to_lowercase() == "unsigned" {
+        ctype.get_numeric_attr_mut().unsigned = Some(true);
+    }
+
+    if tok.is_unquoted() && tok.as_str().to_lowercase() == "zerofill" {
+        ctype.get_numeric_attr_mut().zero_fill = Some(true);
+    }
+
+    return ctype;
+}
+
+fn parse_time_attributes(mut tokens: Vec<Token>, mut ctype: ColumnType) -> ColumnType {
+    if tokens.is_empty() { return ctype; }
+    let tok = tokens.pop().unwrap();
+
+    if tok.is_punctuation() && tok.as_str() == "(" {
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if tok.is_unquoted() && tok.as_str().parse::<u32>().is_ok() {
+            ctype.get_time_attr_mut().fractional = Some(tok.as_str().parse::<u32>().unwrap());
+        }
+
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if !(tok.is_punctuation() && tok.as_str() == ")") { return ctype; }
+    }
+
+    return ctype;
+}
+
+fn parse_string_attributes(mut tokens: Vec<Token>, mut ctype: ColumnType) -> ColumnType {
+    if tokens.is_empty() { return ctype; }
+    let tok = tokens.pop().unwrap();
+
+    if tok.is_punctuation() && tok.as_str() == "(" {
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if tok.is_unquoted() && tok.as_str().parse::<u32>().is_ok() {
+            ctype.get_string_attr_mut().length = Some(tok.as_str().parse::<u32>().unwrap());
+        }
+
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if !(tok.is_punctuation() && tok.as_str() == ")") { return ctype; }
+    }
+    
+    parse_charset_collate(tokens, ctype.get_string_attr_mut());
+
+    return ctype;
+}
+
+fn parse_charset_collate(mut tokens: Vec<Token>, str_attr: &mut StringAttr) {
+    if tokens.is_empty() { return; }
+    let tok = tokens.pop().unwrap();
+
+    if tok.is_unquoted() && tok.as_str().to_lowercase() == "character" {
+        if tokens.is_empty() { return; }
+        let tok = tokens.pop().unwrap();
+
+        if tok.is_unquoted() && tok.as_str().to_lowercase() == "set" {
+            if tokens.is_empty() { return; }
+            let tok = tokens.pop().unwrap();
+
+            if tok.is_unquoted() {
+                str_attr.charset_name = Some(tok.as_str().to_owned());
+            }
+        }
+    }
+
+    if tok.is_unquoted() && tok.as_str().to_lowercase() == "collate" {
+        if tokens.is_empty() { return; }
+        let tok = tokens.pop().unwrap();
+
+        if tok.is_unquoted() {
+            str_attr.collation_name = Some(tok.as_str().to_owned());
+        }
+    }
+}
+
+fn parse_blob_attributes(mut tokens: Vec<Token>, mut ctype: ColumnType) -> ColumnType {
+    if tokens.is_empty() { return ctype; }
+    let tok = tokens.pop().unwrap();
+
+    if tok.is_punctuation() && tok.as_str() == "(" {
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if tok.is_unquoted() && tok.as_str().parse::<u32>().is_ok() {
+            ctype.get_blob_attr_mut().length = Some(tok.as_str().parse::<u32>().unwrap());
+        }
+
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if !(tok.is_punctuation() && tok.as_str() == ")") { return ctype; }
+    }
+
+    return ctype;
+}
+
+fn parse_enum_definition(mut tokens: Vec<Token>, mut ctype: ColumnType) -> ColumnType {
+    if tokens.is_empty() { return ctype; }
+    let tok = tokens.pop().unwrap();
+
+    if tok.is_punctuation() && tok.as_str() == "(" {
+        while !tokens.is_empty() {
+            let tok = tokens.pop().unwrap();
+            if tok.is_quoted() {
+                ctype.get_enum_def_mut().values.push(Tokenizer::unquote(tok.as_str()));
+            }
+        }
+
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if !(tok.is_punctuation() && tok.as_str() == ")") { return ctype; }
+    }
+
+    parse_charset_collate(tokens, &mut ctype.get_enum_def_mut().attr);
+
+    return ctype;
+}
+
+fn parse_set_definition(mut tokens: Vec<Token>, mut ctype: ColumnType) -> ColumnType {
+    if tokens.is_empty() { return ctype; }
+    let tok = tokens.pop().unwrap();
+
+    if tok.is_punctuation() && tok.as_str() == "(" {
+        while !tokens.is_empty() {
+            let tok = tokens.pop().unwrap();
+            if tok.is_quoted() {
+                ctype.get_set_def_mut().members.push(Tokenizer::unquote(tok.as_str()));
+            }
+        }
+
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if !(tok.is_punctuation() && tok.as_str() == ")") { return ctype; }
+    }
+
+    parse_charset_collate(tokens, &mut ctype.get_set_def_mut().attr);
+
+    return ctype;
+}
+
+fn parse_geometry_attributes(mut tokens: Vec<Token>, mut ctype: ColumnType) -> ColumnType {
+    if tokens.is_empty() { return ctype; }
+    let tok = tokens.pop().unwrap();
+
+    if tok.is_unquoted() && tok.as_str().to_lowercase() == "srid" {
+        if tokens.is_empty() { return ctype; }
+        let tok = tokens.pop().unwrap();
+
+        if tok.is_unquoted() && tok.as_str().parse::<u32>().is_ok() {
+            ctype.get_geometry_attr_mut().srid = Some(tok.as_str().parse::<u32>().unwrap());
+        }
+    }
+
+    return ctype;
 }
 
 pub fn parse_column_key(column_key: String) -> ColumnKey {
@@ -48,21 +321,21 @@ pub fn parse_column_extra(expr: String) -> ColumnExtra {
     let mut i = 0;
     while i < words.len() {
         let word = &words[i];
-        match *word {
+        match word.to_lowercase().as_str() {
             "auto_increment" => { extra.auto_increment = true },
             "on" => {
-                if i + 2 < words.len() && words[i + 1] == "update" && words[i + 2] == "CURRENT_TIMESTAMP" {
+                if i + 2 < words.len() && words[i + 1] == "update" && words[i + 2].to_lowercase() == "current_timestamp" {
                     i += 2;
                     extra.on_update_current_timestamp = true;
                 }
             },
-            "STORED" | "VIRTUAL" => {
-                if i + 1 < words.len() && words[i + 1] == "GENERATED" {
+            "stored" | "virtual" => {
+                if i + 1 < words.len() && words[i + 1].to_lowercase() == "generated" {
                     i += 1;
                     extra.generated = true;
                 }
             },
-            "DEFAULT_GENERATED" => {
+            "default_generated" => {
                 extra.default_generated = true;
             },
             _ => {},
