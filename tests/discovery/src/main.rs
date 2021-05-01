9b1b1c57f38132d2f244b658824057232ce68fc2
Chris Tsang <tyt2y7@gmail.com>
2021-05-01 14:14:53 +0800

    Use async_std::main

--- tests/discovery/src/main.rs
+++ tests/discovery/src/main.rs
@@ -1,19 +1,15 @@
-use async_std::task;
 use sea_query::Alias;
 use sea_schema::mysql::discovery::SchemaDiscovery;
 use sqlx::MySqlPool;
 
-fn main() {
+#[async_std::main]
+async fn main() {
 
-    let connection = task::block_on(async {
-        MySqlPool::connect("mysql://sea:sea@localhost/sakila").await.unwrap()
-    });
+    let connection = MySqlPool::connect("mysql://sea:sea@localhost/sakila").await.unwrap();
 
     let schema_discovery = SchemaDiscovery::new(connection, Alias::new("sakila"));
 
-    let schema = task::block_on(async {
-        schema_discovery.discover().await
-    });
+    let schema = schema_discovery.discover().await;
 
     // println!("{}", serde_json::to_string_pretty(&schema).unwrap());
 
