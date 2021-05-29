c718637c06e07f3dc50c5fcd68c241f7298ec2d3
Daniel Lyne <DLyne@pm.me>
2021-05-29 03:28:31 -0400

    Add basic structure of query module for PostgreSQL

--- src/postgresql/mod.rs
+++ src/postgresql/mod.rs
@@ -1,3 +1,7 @@
-#[cfg(feature="def")]
+#[cfg(feature = "def")]
 #[cfg_attr(docsr, doc(cfg(feature = "def")))]
 pub mod def;
+
+#[cfg(feature = "query")]
+#[cfg_attr(docsr, doc(cfg(feature = "query")))]
+pub mod query;
