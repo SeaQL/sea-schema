82bc4997d05a9ca1301cafef43a5a6fd173bf9eb
Daniel Lyne <DLyne@pm.me>
2021-05-29 03:26:02 -0400

    Add basic table and schema definitions

---
+++ src/postgresql/def/schema.rs
@@ -0,0 +1,20 @@
+#[cfg(feature = "with-serde")]
+use serde::{Deserialize, Serialize};
+
+use super::*;
+
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
+pub struct Schema {
+    pub schema: String,
+    pub tables: Vec<TableDef>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
+pub struct TableDef {
+    pub info: TableInfo,
+    pub columns: Vec<ColumnInfo>,
+    pub constraints: Vec<Constraint>,
+    pub inherets: Vec<String>,
+}
