ea3a44f0164497bdaf570bb6032867871cbb12c7
Chris Tsang <tyt2y7@gmail.com>
2021-05-31 14:23:52 +0800

    Feature guard mysql & postgres; fix examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
+#[cfg(feature = "mysql")]
+#[cfg_attr(docsrs, doc(cfg(feature = "mysql")))]
 pub mod mysql;
-pub mod name;
+
+#[cfg(feature = "postgres")]
+#[cfg_attr(docsrs, doc(cfg(feature = "postgres")))]
+pub mod postgres;
+
 pub(crate) mod parser;
-pub mod postgresql;
 pub(crate) mod sqlx_types;
 pub(crate) mod util;
 
+pub mod name;
 pub use name::*;
