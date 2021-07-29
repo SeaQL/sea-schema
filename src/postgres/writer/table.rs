71af023cd4510c6c5d7dfb9fe96a7404a94e6d53
Billy Chan <ccw.billy.123@gmail.com>
2021-07-29 23:02:30 +0800

    WIP

---
+++ src/postgres/writer/table.rs
@@ -0,0 +1,22 @@
+use crate::postgres::def::TableDef;
+use sea_query::{Alias, Iden, Table, TableStatement};
+
+impl TableDef {
+    pub fn write(&self) -> TableStatement {
+        let mut table = Table::create();
+        // table.table(Alias::new(self.info.name.as_ref()));
+        // for col in self.columns.iter() {
+        //     table.col(col.write());
+        // }
+        // table.engine(self.info.engine.to_string().as_str());
+        // table.character_set(self.info.char_set.to_string().as_str());
+        // table.collate(self.info.collation.to_string().as_str());
+        // for idx in self.indexes.iter() {
+        //     table.index(idx.write());
+        // }
+        // for key in self.foreign_keys.iter() {
+        //     table.foreign_key(key.write());
+        // }
+        TableStatement::Create(table)
+    }
+}
---
+++ tests/writer/postgres/src/main.rs
@@ -0,0 +1,19 @@
+use sea_query::PostgresQueryBuilder;
+use sea_schema::postgres::discovery::SchemaDiscovery;
+use sqlx::PgPool;
+
+#[async_std::main]
+async fn main() {
+    let connection = PgPool::connect("postgres://sea:sea@localhost/sakila")
+        .await
+        .unwrap();
+
+    let schema_discovery = SchemaDiscovery::new(connection, "public");
+
+    let schema = schema_discovery.discover().await;
+
+    for table in schema.tables.iter() {
+        println!("{};", table.write().to_string(PostgresQueryBuilder));
+        println!();
+    }
+}
