f776067ec727feb83694c51bd094d28aec96ebb6
Billy Chan <ccw.billy.123@gmail.com>
2021-11-05 16:22:22 +0800

    Strip

--- tests/live/mysql/src/main.rs
+++ tests/live/mysql/src/main.rs
@@ -65,6 +65,7 @@ fn strip_generated_sql(mut sql: String) -> String {
         (Regex::new(r"(?i)MEDIUMINT\(\d+\)").unwrap(), "mediumint"),
         (Regex::new(r"(?i)INT\(\d+\)").unwrap(), "int"),
         (Regex::new(r"(?i)BIGINT\(\d+\)").unwrap(), "bigint"),
+        (Regex::new(r"(?i)LONGTEXE").unwrap(), "json"),
     ] {
         sql = pattern.replace_all(&sql, replacement).to_string();
     }
