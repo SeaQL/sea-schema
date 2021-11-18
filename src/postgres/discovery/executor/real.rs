d70fe565a9bed98afa073339f1881075bb3a2645
Charles Chege <charleschege@protonmail.ch>
2021-11-18 16:40:13 +0300

    Add support for fetching enums

--- src/postgres/discovery/executor/real.rs
+++ src/postgres/discovery/executor/real.rs
@@ -1,5 +1,5 @@
 use sea_query::{PostgresQueryBuilder, SelectStatement};
-use sqlx::{postgres::PgRow, PgPool};
+use sqlx::{postgres::PgRow, PgPool, Row};
 
 sea_query::sea_query_driver_postgres!();
 use sea_query_driver_postgres::bind_query;
@@ -30,5 +30,41 @@ impl Executor {
             .fetch_all(&mut self.pool.acquire().await.unwrap())
             .await
             .unwrap()
+    }
+
+    pub async fn get_enums(&self, select: SelectStatement) -> Vec<EnumColumn> {
+        let (sql, values) = select.build(PostgresQueryBuilder);
+        debug_print!("{}, {:?}", sql, values);
+
+        let query = bind_query(sqlx::query(&sql), &values);
+
+        let rows = query
+            .fetch_all(&mut self.pool.acquire().await.unwrap())
+            .await
+            .unwrap();
+
+        rows.iter()
+            .map(|pg_row| {
+                let column: EnumColumn = pg_row.into();
+
+                column
+            })
+            .collect::<Vec<EnumColumn>>()
+    }
+}
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct EnumColumn {
+    pub name: String,
+    pub value: String,
+}
+
+// #[cfg(feature = "sqlx-postgres")]
+impl From<&PgRow> for EnumColumn {
+    fn from(row: &PgRow) -> Self {
+        Self {
+            name: row.get(0),
+            value: row.get(1),
+        }
     }
 }
