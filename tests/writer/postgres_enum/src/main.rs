beabb80ae330d0443730fca54826d47694b2f251
Charles Chege <charleschege@protonmail.ch>
2021-11-19 09:48:20 +0300

    Test case to discover all the enums in a given database schema
    Write into SQL and then parse the raw SQL asserting that all
    discovered enums are the same as their raw SQL queries

--- tests/writer/postgres_enum/src/main.rs
+++ tests/writer/postgres_enum/src/main.rs
@@ -1,4 +1,4 @@
-use sea_schema::postgres::def::{EnumDef, PgEnum, StringAttr, Type};
+use sea_schema::postgres::{def::Type, discovery::SchemaDiscovery};
 use sqlx::PgPool;
 
 #[async_std::main]
@@ -7,84 +7,14 @@ async fn main() {
         .await
         .unwrap();
 
-    let rows: Vec<PgEnum> = sqlx::query_as("SELECT pg_type.typname, pg_enum.enumlabel FROM pg_type JOIN pg_enum ON pg_enum.enumtypid = pg_type.oid;",
-    )
-    .fetch_all(&connection)
-    .await
-    .unwrap();
+    let enums_discovery = SchemaDiscovery::new(connection, "public")
+        .discover_enums()
+        .await;
 
-    let custom_enum = PgEnum::new().to_enum_type(rows);
-
-    let font_family = r#"CREATE TYPE "font_family" AS ENUM ('serif', 'sans', 'monospace')"#;
-
-    let rating = r#"CREATE TYPE "mpaa_rating" AS ENUM ('G', 'PG', 'PG-13', 'R', 'NC-17')"#;
-
-    let parse_font_family = Type::enum_from_query(font_family);
-    let parse_rating = Type::enum_from_query(rating);
-
-    assert_eq!(
-        parse_font_family,
-        Type::Enum(EnumDef {
-            values: vec![
-                "serif".to_owned(),
-                "sans".to_owned(),
-                "monospace".to_owned(),
-            ],
-            attr: StringAttr { length: Some(11,) },
-            typename: "font_family".to_owned(),
-        },)
-    );
-
-    // Check if the end of parsed statement is same as the sql query
-    assert_eq!(
-        match parse_font_family {
-            Type::Enum(enum_def) => Some(enum_def.to_sql_query()),
-            _ => None,
-        },
-        Some(font_family.to_owned())
-    );
-
-    assert_eq!(
-        parse_rating,
-        Type::Enum(EnumDef {
-            values: vec![
-                "G".to_owned(),
-                "PG".to_owned(),
-                "PG-13".to_owned(),
-                "R".to_owned(),
-                "NC-17".to_owned(),
-            ],
-            attr: StringAttr { length: Some(11,) },
-            typename: "mpaa_rating".to_owned(),
-        },)
-    );
-
-    // Check if the end of parsed statement is same as the sql query
-    assert_eq!(
-        match parse_rating {
-            Type::Enum(enum_def) => Some(enum_def.to_sql_query()),
-            _ => None,
-        },
-        Some(rating.to_owned())
-    );
-
-    assert_eq!(
-        custom_enum.enum_to_create_statement(),
-        Some(r#"CREATE TYPE "mpaa_rating" AS ENUM ('G', 'PG', 'PG-13', 'R', 'NC-17')"#.to_owned())
-    );
-
-    assert_eq!(
-        custom_enum,
-        Type::Enum(EnumDef {
-            values: vec![
-                "G".to_owned(),
-                "PG".to_owned(),
-                "PG-13".to_owned(),
-                "R".to_owned(),
-                "NC-17".to_owned(),
-            ],
-            attr: StringAttr { length: Some(11,) },
-            typename: "mpaa_rating".to_owned(),
-        },)
-    );
+    // Assert that all enums in the database are equal to their
+    // raw SQL queries
+    enums_discovery.iter().for_each(|enum_type| {
+        let to_sql_query = enum_type.get_enum_def().to_sql_query();
+        assert_eq!(enum_type, &Type::enum_from_query(&to_sql_query));
+    });
 }
