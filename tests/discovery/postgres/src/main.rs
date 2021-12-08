3ee593d0c79a7df61abe0a3de1034629bbc1be2f
Billy Chan <ccw.billy.123@gmail.com>
2021-12-08 14:45:08 +0800

    Add musicbrainz test cases

--- tests/discovery/postgres/src/main.rs
+++ tests/discovery/postgres/src/main.rs
@@ -1,17 +1,29 @@
 use sea_schema::postgres::discovery::SchemaDiscovery;
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions, PgPool};
+use std::time::Duration;
 
 #[async_std::main]
 async fn main() {
-    let connection = PgPool::connect("postgres://sea:sea@localhost/sakila")
-        .await
-        .unwrap();
-
-    let schema_discovery = SchemaDiscovery::new(connection, "public");
-
-    let schema = schema_discovery.discover().await;
-
-    // println!("{}", serde_json::to_string_pretty(&schema).unwrap());
-
-    println!("{:#?}", schema);
+    {
+        // Testing discovery on `sakila` database
+        let connection = PgPool::connect("postgres://sea:sea@localhost/sakila")
+            .await
+            .unwrap();
+        let schema_discovery = SchemaDiscovery::new(connection, "public");
+        let schema = schema_discovery.discover().await;
+        // println!("{}", serde_json::to_string_pretty(&schema).unwrap());
+        println!("{:#?}", schema);
+    }
+    {
+        // Testing discovery on `musicbrainz` database
+        let connection = PgPoolOptions::new()
+            .max_connections(80)
+            .connect_timeout(Duration::from_secs(600))
+            .connect("postgres://sea:sea@localhost/musicbrainz")
+            .await
+            .unwrap();
+        let schema_discovery = SchemaDiscovery::new(connection, "public");
+        let schema = schema_discovery.discover().await;
+        println!("{:#?}", schema);
+    }
 }
--- tests/writer/postgres/src/main.rs
+++ tests/writer/postgres/src/main.rs
@@ -1,19 +1,34 @@
-use sea_schema::postgres::discovery::SchemaDiscovery;
-use sea_schema::sea_query::PostgresQueryBuilder;
-use sqlx::PgPool;
+use sea_schema::{postgres::discovery::SchemaDiscovery, sea_query::PostgresQueryBuilder};
+use sqlx::{postgres::PgPoolOptions, PgPool};
+use std::time::Duration;
 
 #[async_std::main]
 async fn main() {
-    let connection = PgPool::connect("postgres://sea:sea@localhost/sakila")
-        .await
-        .unwrap();
-
-    let schema_discovery = SchemaDiscovery::new(connection, "public");
-
-    let schema = schema_discovery.discover().await;
-
-    for table in schema.tables.iter() {
-        println!("{};", table.write().to_string(PostgresQueryBuilder));
-        println!();
+    {
+        // Testing writer on `sakila` database
+        let connection = PgPool::connect("postgres://sea:sea@localhost/sakila")
+            .await
+            .unwrap();
+        let schema_discovery = SchemaDiscovery::new(connection, "public");
+        let schema = schema_discovery.discover().await;
+        for table in schema.tables.iter() {
+            println!("{};", table.write().to_string(PostgresQueryBuilder));
+            println!();
+        }
+    }
+    {
+        // Testing writer on `musicbrainz` database
+        let connection = PgPoolOptions::new()
+            .max_connections(80)
+            .connect_timeout(Duration::from_secs(600))
+            .connect("postgres://sea:sea@localhost/musicbrainz")
+            .await
+            .unwrap();
+        let schema_discovery = SchemaDiscovery::new(connection, "public");
+        let schema = schema_discovery.discover().await;
+        for table in schema.tables.iter() {
+            println!("{};", table.write().to_string(PostgresQueryBuilder));
+            println!();
+        }
     }
 }
