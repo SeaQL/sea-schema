35c3646ee2fd01decdf3eb103b369f13ec025a16
Billy Chan <ccw.billy.123@gmail.com>
2021-12-09 19:34:16 +0800

    Add assertions

--- tests/writer/postgres/src/main.rs
+++ tests/writer/postgres/src/main.rs
@@ -1,6 +1,7 @@
+use pretty_assertions::assert_eq;
 use sea_schema::{postgres::discovery::SchemaDiscovery, sea_query::PostgresQueryBuilder};
 use sqlx::{postgres::PgPoolOptions, PgPool};
-use std::time::Duration;
+use std::{collections::HashMap, time::Duration};
 
 #[async_std::main]
 async fn main() {
@@ -26,9 +27,34 @@ async fn main() {
             .unwrap();
         let schema_discovery = SchemaDiscovery::new(connection, "public");
         let schema = schema_discovery.discover().await;
+        let mut stmts = HashMap::new();
         for table in schema.tables.iter() {
-            println!("{};", table.write().to_string(PostgresQueryBuilder));
+            let stmt = table.write().to_string(PostgresQueryBuilder);
+            println!("{};", stmt);
             println!();
+            stmts.insert(table.info.name.as_str(), stmt);
         }
+        assert_eq!(
+            stmts
+                .get("artist_release")
+                .expect("Create 'artist_release' table statament not found")
+                .as_str(),
+            [
+                r#"CREATE TABLE "artist_release" ("#,
+                r#""is_track_artist" bool NOT NULL,"#,
+                r#""artist" integer NOT NULL,"#,
+                r#""first_release_date" integer,"#,
+                r#""catalog_numbers" array,"#,
+                r#""country_code" char(2),"#,
+                r#""barcode" bigint,"#,
+                r#""sort_character" char(1) NOT NULL,"#,
+                r#""release" integer NOT NULL,"#,
+                r#"CONSTRAINT "artist_release_fk_artist" FOREIGN KEY ("artist") REFERENCES "artist" ("id") ON DELETE CASCADE ON UPDATE NO ACTION,"#,
+                r#"CONSTRAINT "artist_release_fk_release" FOREIGN KEY ("release") REFERENCES "release" ("id") ON DELETE CASCADE ON UPDATE NO ACTION"#,
+                r#")"#,
+            ]
+            .join(" ")
+            .as_str()
+        );
     }
 }
