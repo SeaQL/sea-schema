e6dff1b0e643c33dfc17d124409d35bb0eb2669b
Charles Chege <charleschege@protonmail.ch>
2021-12-11 16:42:25 +0300

    Modify test to use new methods

--- tests/discovery/sqlite/src/main.rs
+++ tests/discovery/sqlite/src/main.rs
@@ -1,13 +1,13 @@
-use sea_schema::sqlite::{SchemaDiscovery, DiscoveryResult};
 use sea_schema::sea_query::SqliteQueryBuilder;
-
+use sea_schema::sqlite::{DiscoveryResult, SchemaDiscovery};
+use sqlx::SqlitePool;
 #[async_std::main]
 async fn main() -> DiscoveryResult<()> {
-    // let connection = SqlitePool::connect("sqlite://tests/sakila/sqlite/sakila.db")
-    //     .await
-    //     .unwrap();
+    let connection = SqlitePool::connect("sqlite://tests/sakila/sqlite/sakila.db")
+        .await
+        .unwrap();
 
-    let mut schema_discovery = SchemaDiscovery::new("tests/sakila/sqlite/sakila.db").await?;
+    let mut schema_discovery = SchemaDiscovery::new(connection);
 
     let schema = schema_discovery.discover().await?;
 
