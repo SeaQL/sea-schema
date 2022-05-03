644741a1758c43d092de20e670cc14c5bc0e4e67
Billy Chan <ccw.billy.123@gmail.com>
2022-05-03 17:54:06 +0800

    Remove clap dependency, rewrite `get_cli_subcommands` to macros

--- src/migration/cli.rs
+++ src/migration/cli.rs
@@ -1,34 +1,35 @@
 //! Migrator CLI utility
 
-use clap::{App, Arg, SubCommand};
-
 /// Migrator CLI subcommands
-pub fn get_subcommands() -> Vec<App<'static, 'static>> {
-    vec![
-        SubCommand::with_name("fresh")
-            .about("Drop all tables from the database, then reapply all migrations"),
-        SubCommand::with_name("refresh")
-            .about("Rollback all applied migrations, then reapply all migrations"),
-        SubCommand::with_name("reset").about("Rollback all applied migrations"),
-        SubCommand::with_name("status").about("Check the status of all migrations"),
-        SubCommand::with_name("up")
-            .about("Apply pending migrations")
-            .arg(
-                Arg::with_name("NUM_MIGRATION")
-                    .long("num")
-                    .short("n")
-                    .help("Number of pending migrations to be applied")
-                    .takes_value(true),
-            ),
-        SubCommand::with_name("down")
-            .about("Rollback applied migrations")
-            .arg(
-                Arg::with_name("NUM_MIGRATION")
-                    .long("num")
-                    .short("n")
-                    .help("Number of pending migrations to be rolled back")
-                    .takes_value(true)
-                    .default_value("1"),
-            ),
-    ]
+#[macro_export]
+macro_rules! get_cli_subcommands {
+    () => {
+        [
+            clap::SubCommand::with_name("fresh")
+                .about("Drop all tables from the database, then reapply all migrations"),
+            clap::SubCommand::with_name("refresh")
+                .about("Rollback all applied migrations, then reapply all migrations"),
+            clap::SubCommand::with_name("reset").about("Rollback all applied migrations"),
+            clap::SubCommand::with_name("status").about("Check the status of all migrations"),
+            clap::SubCommand::with_name("up")
+                .about("Apply pending migrations")
+                .arg(
+                    clap::Arg::with_name("NUM_MIGRATION")
+                        .long("num")
+                        .short("n")
+                        .help("Number of pending migrations to be applied")
+                        .takes_value(true),
+                ),
+            clap::SubCommand::with_name("down")
+                .about("Rollback applied migrations")
+                .arg(
+                    clap::Arg::with_name("NUM_MIGRATION")
+                        .long("num")
+                        .short("n")
+                        .help("Number of pending migrations to be rolled back")
+                        .takes_value(true)
+                        .default_value("1"),
+                ),
+        ]
+    };
 }
