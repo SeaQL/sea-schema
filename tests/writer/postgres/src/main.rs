268a874d1aa96c362a435c278652c50ba25a83d6
Billy Chan <ccw.billy.123@gmail.com>
2022-10-20 17:48:08 +0800

    Revert debug code

--- tests/writer/postgres/src/main.rs
+++ tests/writer/postgres/src/main.rs
@@ -4,10 +4,10 @@ use sqlx::PgPool;
 
 #[async_std::main]
 async fn main() {
-    env_logger::builder()
-        .filter_level(log::LevelFilter::Debug)
-        .is_test(true)
-        .init();
+    // env_logger::builder()
+    //     .filter_level(log::LevelFilter::Debug)
+    //     .is_test(true)
+    //     .init();
 
     let url = std::env::var("DATABASE_URL_SAKILA")
         .unwrap_or("postgres://root:root@localhost/sakila".to_owned());
