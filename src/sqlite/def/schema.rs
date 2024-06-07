7c7764a37f9bb9eb4619775ab6204eca6fa8c202
Billy Chan <ccw.billy.123@gmail.com>
2024-06-07 17:16:42 +0800

    SQLite collect_unique_indexes

--- src/sqlite/def/schema.rs
+++ src/sqlite/def/schema.rs
@@ -5,3 +5,16 @@ pub struct Schema {
     pub tables: Vec<TableDef>,
     pub indexes: Vec<IndexInfo>,
 }
+
+impl Schema {
+    pub fn collect_unique_indexes(mut self) -> Self {
+        for table in self.tables.iter_mut() {
+            for index in self.indexes.iter() {
+                if index.unique && index.table_name == table.name {
+                    table.constraints.push(index.clone());
+                }
+            }
+        }
+        self
+    }
+}
